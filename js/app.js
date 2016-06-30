404e7422100d9f70042acf0e156b44be5558b4c6
RayJustin <Ray.Justin@live.com>
2016-06-30 02:41:47 -0700

    Update 2

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
 $('document').ready(function(){
 
-$('form').on('submit', function(event){
-	event.preventDefault();
-	var contact = createObject();
-	console.log(contact.firstname);
-}); 
+	var id = 0
 
+	$('form').on('submit', function(event){
+		event.preventDefault();
+		var contact = createObject();
+		contact.id = id;
+		$('.contactList').append(' <li id="'+ contact.id +'"> ' + contact.firstname + ' ' + contact.lastname + ' </li> ');
+		$(this)[0].reset('');
+		id++;
+		console.log(contact);
+	}); 
+
+	// Must supply the 'li' as a parameter when the li isn't on the page when the code initially runs.
+	$('ul').on('click', 'li', function(){
+		
+		// Need to load the values of the object back into their respective inputs
+	});
 });
 
 
@@ -16,8 +27,8 @@ function createObject() {
  		contact[fields[i].name] = fields[i].value;
 	}
 	return contact;
-};
+}
 
-
-
+// function updateContact() {}
+	
 
